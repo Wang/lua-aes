d3266a4e84865fc9bf834215027b7df496fea044
guoqiang <dudu@DududeMacBook-Pro.local>
2016-08-15 16:44:05 +0800

    修改加解密补充\0问题

--- resty/ecb_mcrypt.lua
+++ resty/ecb_mcrypt.lua
@@ -37,9 +37,9 @@ int mcrypt_enc_is_block_mode(MCRYPT td);
 int mcrypt_enc_get_block_size(MCRYPT td);
 ]]
 
-local mcrypt = ffi.load('libmcrypt.so.4')
+local mcrypt = ffi.load("libmcrypt")
 
-_M.new = function (self)
+_M.new = function (self, key)
     local cipher = 'rijndael-128'
     local mode = 'ecb'
 
@@ -47,130 +47,103 @@ _M.new = function (self)
     local c_mode 	=	ffi_new("char[4]", mode)
 
     local td = mcrypt.mcrypt_module_open(c_cipher, nil, c_mode, nil)
-    return setmetatable( { _td = td }, mt )
-end
-
-
-_M.pass = function (self, key, raw,enc_or_dec)
-
-		local dencrypt	= enc_or_dec
-    local iv_len = 8
-    local cipher = 'rijndael-128'
-    local mode = 'ecb'
-
-    local c_cipher 	=	ffi_new("char[?]",#cipher+1, cipher)
-    local c_mode 	=	ffi_new("char[4]", mode)
-		local td = mcrypt.mcrypt_module_open(c_cipher, nil, c_mode, nil)
-
-		if  td ==0  then
-			ngx.log(ngx.ERR , "mcrypt_module_open failed")
-			return nil
-		end
-
-    local iv_key =	"1234567890123456";
-		local key_len=  #key;
-		local data_len=  #raw;
-
-		local block_size, max_key_length, use_key_length, i, count, iv_size;
-		--/* Checking for key-length */
-		max_key_length = mcrypt.mcrypt_enc_get_key_size(td);
-		if  key_len > max_key_length  then
-			ngx.log(ngx.ERR , "Size of key is too large for this algorithm key_len:",key_len,",max_key:",max_key_length)
-			return nil
-		end
-
-		count 	=	ffi_new("int[1]")
-		local key_size_tmp = mcrypt.mcrypt_enc_get_supported_key_sizes(td, count);
-		local key_length_sizes = ffi.cast("int *",key_size_tmp)
-
-		local key_s	=	nil;
-
-		if count[0] == 0 and key_length_sizes == nil then --/* all lengths 1 - k_l_s = OK */
-			use_key_length = key_len;
-			key_s = ffi_new("unsigned char[?]",use_key_length,key)
-		end
-
-	if  count[0] == 1 then
-		key_s = ffi_new("char[?]",key_length_sizes[0])
-		ffi.fill(key_s ,use_key_length,0);
-		ffi.copy(key_s, key, math.min(key_len, key_length_sizes[0]));
-		use_key_length = key_length_sizes[0];
-	 else
-		use_key_length = max_key_length;
-
-		for i=0,count[0]-1 do
-			if  key_length_sizes[i] >= key_len and	key_length_sizes[i] < use_key_length then
-				use_key_length = key_length_sizes[i];
-			end
-		end
-		key_s = ffi_new("char[?]",use_key_length)
-
-		ffi.copy(key_s ,key, math.min(key_len,use_key_length));
+	if  td ==0  then
+		ngx.log(ngx.ERR , "mcrypt_module_open failed")
+		return nil
 	end
 
-
-
-	if key_length_sizes~=nil then
-		mcrypt.mcrypt_free(key_length_sizes);
+	local max_key_length = mcrypt.mcrypt_enc_get_key_size(td);
+	if  #(key) > max_key_length  then
+		ngx.log(ngx.ERR , "Size of key is too large for this algorithm key_len:",key_len,",max_key:",max_key_length)
+		return nil
 	end
+	local key_sizes = {16, 24, 32}
+	local use_key_length = max_key_length
+	for  k in ipairs(key_sizes) do
+		if key_sizes[k] >= #(key) then
+			use_key_length = key_sizes[k]
+			break
+		end
+	end
+	local key_s = ffi_new("char[?]",use_key_length)
+	ffi.copy(key_s ,key, math.min(#(key), use_key_length));
 
-	local iv_s = nil;
-	local  iv_size = mcrypt.mcrypt_enc_get_iv_size (td);
-
-	local has_iv = mcrypt.mcrypt_enc_mode_has_iv(td) ;
-
-
-	local data_size	=	0;
 	local block = mcrypt.mcrypt_enc_is_block_mode(td);
+	local block_size = nil
 	if  block == 1 then
 		block_size =	mcrypt.mcrypt_enc_get_block_size(td);
-		data_size = math.floor(((data_len - 1) / block_size) + 1) * block_size;
-
-	else
-		data_size = data_len;
 	end
 
-
-	local data_s = ffi_new("char[?]",data_size)
-	ffi.fill(data_s ,data_size,0);
-	ffi.copy(data_s ,raw ,data_len);
-
-	local ini_ret = mcrypt.mcrypt_generic_init(td, key_s, use_key_length, c_iv)
+	local ini_ret = mcrypt.mcrypt_generic_init(td, key_s, use_key_length, nil)
 	if ini_ret < 0 then
 		ngx.log(ngx.ERR , "Mcrypt initialisation failed");
 		ngx.say(  ini_ret,"ini_ret initialisation failed");
 		return nil
 	end
+	mcrypt.mcrypt_generic_deinit(td)
+	mcrypt.mcrypt_module_close(td)
 
- 
+    return setmetatable( { 
+		_key = key,
+		_key_chars = key_s,
+		_key_len = use_key_length,
+		_block_size = block_size,
+		_cipher = c_cipher,
+		_mode = c_mode
+		 }, mt )
+end
+
+
+_M.pass = function (self, raw,enc_or_dec)
+	local dencrypt	= enc_or_dec
+	local data_len, data_size = #raw;
+
+	if  self._block_size ~= nil then
+		data_size = math.floor(((data_len - 1) / self._block_size) + 1) * self._block_size;
+	end
+
+	local data_s = ffi_new("char[?]",data_size)
+	ffi.fill(data_s ,data_size,0);
+	ffi.copy(data_s ,raw ,data_len);
+
+    local td = mcrypt.mcrypt_module_open(self._cipher, nil, self._mode, nil)
+	if  td == 0  then
+		ngx.log(ngx.ERR , "mcrypt_module_open failed")
+		return nil
+	end
+	local ini_ret = mcrypt.mcrypt_generic_init(td, self._key_chars, self._key_len, nil)
+	if ini_ret < 0 then
+		ngx.log(ngx.ERR , "Mcrypt initialisation failed");
+		return nil
+	end
 
 	if  dencrypt == 1 then
 		mcrypt.mcrypt_generic(td, data_s, data_size);
 	else
 		mcrypt.mdecrypt_generic(td, data_s, data_size);
 	end
+	mcrypt.mcrypt_generic_deinit(td)
+	mcrypt.mcrypt_module_close(td)
 
-	local ret_str = ffi_str(data_s,data_size);
-
-	mcrypt.mcrypt_generic_end(td);
-
+	local pad_size = 0
+	for i=data_size-1, 0, -1 do
+		if data_s[i] == 0 then
+			pad_size = pad_size + 1
+		else
+			break
+		end
+	end
+	local ret_str = ffi_str(data_s,data_size - pad_size);
 
 	return ret_str
 end
 
-_M.encrypt = function (self, key, raw)
-	return _M.pass(self, key, raw,1);
+_M.encrypt = function (self, raw)
+	return _M.pass(self, raw, 1);
 end
 
-_M.decrypt = function(self, key, raw)
-	return _M.pass(self, key, raw,0);
-end
-
-_M.close = function(self)
-    local td = self._td
-    if td then
-        mcrypt.mcrypt_module_close(td)
-     end
+_M.decrypt = function(self, raw)
+	return _M.pass(self, raw, 0);
 end
 
 return _M
